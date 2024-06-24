973c962677e8769263c251a95c1a9088473e8f12
Frank Marazita <fmarazita1@gmail.com>
2024-06-25 00:44:22 +1000

    simple resizable panels

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
+import { Panel, PanelGroup, PanelResizeHandle } from "react-resizable-panels"
+
 function App() {
   return (
-    <div className="flex flex-col flex-grow gap-4">
-      <div className="flex flex-col flex-grow bg-blue-400"></div>
-      <div className="flex flex-col flex-grow bg-blue-400"></div>
-      <div className="flex flex-col flex-grow bg-blue-400"></div>
-      <div className="flex flex-col flex-grow bg-blue-400"></div>
+    <div className="flex flex-col flex-grow">
+      <PanelGroup direction="horizontal" className="gap-1">
+        <Panel minSize={5} className="flex flex-col flex-grow bg-blue-400">
+        </Panel>
+        <PanelResizeHandle />
+        <Panel defaultSize={70} className="flex flex-col flex-grow bg-blue-400">
+        </Panel>
+        <PanelResizeHandle />
+        <Panel minSize={5} className="flex flex-col flex-grow bg-blue-400">
+        </Panel>
+      </PanelGroup>
     </div>
   )
 }
