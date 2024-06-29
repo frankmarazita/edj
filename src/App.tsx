dafb5bb54f2461401594b6d61aeea7ea9b5f1661
Frank Marazita <fmarazita1@gmail.com>
2024-06-29 17:30:33 +1000

    layout tinkering

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,43 @@ import { Panel, PanelGroup, PanelResizeHandle } from "react-resizable-panels"
 
 function App() {
   return (
-    <div className="flex flex-col flex-grow">
-      <PanelGroup direction="horizontal" className="gap-1">
-        <Panel minSize={5} className="flex flex-col flex-grow bg-blue-400">
-        </Panel>
-        <PanelResizeHandle />
-        <Panel defaultSize={70} className="flex flex-col flex-grow bg-blue-400">
-        </Panel>
-        <PanelResizeHandle />
-        <Panel minSize={5} className="flex flex-col flex-grow bg-blue-400">
-        </Panel>
-      </PanelGroup>
-    </div>
+    <PanelGroup direction="horizontal" className="gap-1">
+      <Panel minSize={5} className="flex flex-col flex-grow bg-blue-400">
+      </Panel>
+
+      <PanelResizeHandle />
+
+      <Panel defaultSize={70} className="gap-1">
+        <PanelGroup direction="vertical" className="gap-1">
+          <Panel minSize={30} className="flex flex-col flex-grow bg-blue-400">
+          </Panel>
+
+          <PanelResizeHandle />
+
+          <Panel defaultSize={70} className="flex flex-col flex-grow ">
+          <PanelGroup direction="horizontal" className="gap-1">
+            <Panel minSize={15} className="flex flex-col flex-grow bg-blue-400">
+            </Panel>
+
+            <PanelResizeHandle />
+
+            <Panel defaultSize={70} className="flex flex-col flex-grow bg-blue-400">
+            </Panel>
+
+            <PanelResizeHandle />
+
+            <Panel minSize={15} className="flex flex-col flex-grow bg-blue-400">
+            </Panel>
+          </PanelGroup>
+          </Panel>
+        </PanelGroup>
+      </Panel>
+
+      <PanelResizeHandle />
+
+      <Panel minSize={5} className="flex flex-col flex-grow bg-blue-400">
+      </Panel>
+    </PanelGroup>
   )
 }
 
